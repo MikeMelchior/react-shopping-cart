9666f76ad6ecd8c65f079fbba174ee4d268dfe83
Mike Melchior <mike.melchior@outlook.com>
2023-04-21 11:21:50 -0400

    add images to shop

--- src/Components/Shop.js
+++ src/Components/Shop.js
@@ -1,6 +1,17 @@
 import React, { useEffect, useId, useState } from 'react'
 import Cart from './Cart'
 import uniqid from 'uniqid'
+import apple from '../images/apple.png'
+import cucumber from '../images/cucumber.png'
+import banana from '../images/banana.png'
+import onion from '../images/onion.png'
+import carrot from '../images/carrot.png'
+import broccoli from '../images/broccoli.png'
+import spinach from '../images/spinach.png'
+import lemon from '../images/lemon.png'
+import mango from '../images/mango.png'
+import avocado from '../images/avocado.png'
+
 
 function Shop() {
   const [products, setProducts] = useState([])
@@ -8,16 +19,16 @@ function Shop() {
 
   useEffect(() => {
     const inventory = [
-      { name: 'apple', cost: 0.5 },
-      { name: 'cucumber', cost: 2 },
-      { name: 'banana', cost: 1.25 },
-      { name: 'peppers', cost: 3.8 },
-      { name: 'carrot', cost: 1 },
-      { name: 'broccoli', cost: 1.80 },
-      { name: 'spinach', cost: 4.1 },
-      { name: 'lemon', cost: 0.8 },
-      { name: 'mango', cost: 2 },
-      { name: 'avocado', cost: 0.25 },
+      { name: 'apple', cost: 0.5, img: apple},
+      { name: 'cucumber', cost: 2, img: cucumber },
+      { name: 'banana', cost: 1.25, img: banana },
+      { name: 'onion', cost: 3.8, img: onion },
+      { name: 'carrot', cost: 1, img: carrot },
+      { name: 'broccoli', cost: 1.80, img: broccoli },
+      { name: 'spinach', cost: 4.1, img: spinach },
+      { name: 'lemon', cost: 0.8, img: lemon },
+      { name: 'mango', cost: 2, img: mango },
+      { name: 'avocado', cost: 0.25, img: avocado },
     ]
     setProducts(inventory)
   }, [])
@@ -27,9 +38,16 @@ function Shop() {
     <div className='shop'>
       <Cart />
       <h1>Produce Store</h1>
-      {products.map(p => {
-        return <p key={uniqid()}>{p.name}: {p.cost}</p>
-      })}
+      <div className="items">
+        {products.map(p => (
+          <div key={uniqid()} className='item'>
+            <img id='product-image' src={p.img} alt="food" />
+            <p>{p.name}</p>
+            <p>{p.cost}</p>
+          </div>
+        ))}
+      </div>
+      
     </div>
   )
 }
