c087b00dadb90555157de2afa71b47b1801055eb
Mike Melchior <mike.melchior@outlook.com>
2023-04-21 14:50:23 -0400

    add details card functionality

--- src/Components/Shop.js
+++ src/Components/Shop.js
@@ -11,49 +11,71 @@ import spinach from '../images/spinach.png'
 import lemon from '../images/lemon.png'
 import mango from '../images/mango.png'
 import avocado from '../images/avocado.png'
+import ItemCard from './ItemCard'
 
 
 function Shop() {
   const [products, setProducts] = useState([])
-  
+  const [item, setItem] = useState({});
+  const [showCard, setShowCard] = useState(false)
 
   useEffect(() => {
     const inventory = [
-      { name: 'apple', cost: 0.50, img: apple},
-      { name: 'cucumber', cost: 2.00, img: cucumber },
-      { name: 'banana', cost: 1.25, img: banana },
-      { name: 'onion', cost: 3.80, img: onion },
-      { name: 'carrot', cost: 1.00, img: carrot },
-      { name: 'broccoli', cost: 1.80, img: broccoli },
-      { name: 'spinach', cost: 4.10, img: spinach },
-      { name: 'lemon', cost: 0.80, img: lemon },
-      { name: 'mango', cost: 2.00, img: mango },
-      { name: 'avocado', cost: 0.25, img: avocado },
+      { name: 'apple', cost: 0.50, img: apple, quantity: 0},
+      { name: 'cucumber', cost: 2.00, img: cucumber, quantity: 0 },
+      { name: 'banana', cost: 1.25, img: banana, quantity: 0 },
+      { name: 'onion', cost: 3.80, img: onion, quantity: 0 },
+      { name: 'carrot', cost: 1.00, img: carrot, quantity: 0 },
+      { name: 'broccoli', cost: 1.80, img: broccoli, quantity: 0 },
+      { name: 'spinach', cost: 4.10, img: spinach, quantity: 0 },
+      { name: 'lemon', cost: 0.80, img: lemon, quantity: 0 },
+      { name: 'mango', cost: 2.00, img: mango, quantity: 0 },
+      { name: 'avocado', cost: 0.25, img: avocado, quantity: 0 },
     ]
     setProducts(inventory)
   }, [])
 
+  const getItem = (product) => {
+    const itemDetails = products.find(p => p.name === product.name)
+    setItem(itemDetails)
+  }
+
+  const displayItem = (product) => {
+    getItem(product)
+    setShowCard(true)
+  }
+
   
   return (
-    <div className='shop'>
-      <Nav />
-      {/* <Cart /> */}
-      <h1>Local Produce</h1>
-      <div className="items">
-        {products.map(p => (
-          <div key={uniqid()} className='item'> 
-            <p className='item-title'>{p.name}</p>
-            <img id='product-image' src={p.img} alt="food" />
-            <div className='item-handler'>
-              <p>${p.cost}</p>
-
+    <>
+      {showCard 
+        ? <ItemCard item={item} setShowCard={setShowCard}/>
+        
+        : <div className='shop'>
+            <Nav />
+            <h1>Local Produce</h1>
+            <div className="items">
+              {products.map(p => (
+                <div 
+                  key={uniqid()} 
+                  className='item'
+                > 
+                  <p className='item-title'>{p.name}</p>
+                  <img 
+                    id='product-image' 
+                    src={p.img} 
+                    alt="food" 
+                    onClick={() => displayItem(p)}
+                  />
+                  <div className='item-handler'>
+                    <p>${p.cost}</p>
+                  </div>
+                </div>
+              ))}
             </div>
-            
           </div>
-        ))}
-      </div>
-      
-    </div>
+      }
+    </>
   )
 }
 
