435852f59d9069cefdc10c5a51c0832bc55dd52e
Mike Melchior <mike.melchior@outlook.com>
2023-04-21 14:49:41 -0400

    add mock social image links

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,16 +1,24 @@
 import React from 'react'
 import Nav from './Nav'
+import insta from '../images/insta.png'
+import facebook from '../images/facebook.png'
+import twitter from '../images/twitter.png'
 
 
 function Contact() {
   return (
     <div className='contact'>
       <Nav />
-      <h2>We would love to hear your feedback!</h2>
-      <div>
         <p>Phone: (123) 456 - 7890</p>
         <p>Email: shoplocalproduce@efake.com</p>
-        <p></p>
+      <div className='socials-container'>
+        <h2>We would love to hear your feedback!</h2>
+        <div className='socials'>
+          <img id='insta' src={insta} alt="instagram" />
+          <img src={facebook} alt="facebook" />
+          <img src={twitter} alt="twitter" />
+        </div>
+        
       </div>
     </div>
   )
