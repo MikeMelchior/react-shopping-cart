0aadae161c5552c8dfa9615e5f2285008875080c
Mike Melchior <mike.melchior@outlook.com>
2023-04-24 09:43:10 -0400

    rework nav bar

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,17 +1,28 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import '../styles.css'
+import cart from '../images/cart.png'
 
 function Nav() {
   
-  
-
-
   return (
     <div className='nav'>
-      <div className='link'><Link to={'/'}>Home</Link></div>
-      <div className='link'><Link to={'/shop'}>Shop Produce</Link></div>
-      <div className='link'><Link to={'/contact'}>Contact Us</Link></div>
+      <ul>
+        <li>
+          <Link to={'/'}>Home</Link>
+        </li>
+        <li>
+          <Link to={'/shop'} >Shop Produce</Link>
+        </li>
+        <li>
+          <Link to={'/contact'}>Contact Us</Link>
+        </li>
+        <li>
+          <Link to={'/cart'}>
+            <img id='cart' src={cart} alt="cart" />
+          </Link>
+        </li>
+      </ul>
     </div>
   )
 }
