e0e58bb8bdff5d4d0ae29501c52e8617e26aec61
Mike Melchior <mike.melchior@outlook.com>
2023-04-24 09:42:42 -0400

    use data file for shop inventory

---
+++ src/Inventory/data.js
@@ -0,0 +1,26 @@
+import apple from '../images/apple.png'
+import cucumber from '../images/cucumber.png'
+import banana from '../images/banana.png'
+import onion from '../images/onion.png'
+import carrot from '../images/carrot.png'
+import broccoli from '../images/broccoli.png'
+import spinach from '../images/spinach.png'
+import lemon from '../images/lemon.png'
+import mango from '../images/mango.png'
+import avocado from '../images/avocado.png'
+
+
+const inventory = [
+  { name: 'apple', cost: 0.50, img: apple},
+  { name: 'cucumbers', cost: 2.00, img: cucumber},
+  { name: 'bananas', cost: 1.25, img: banana},
+  { name: 'onions', cost: 3.80, img: onion},
+  { name: 'carrots', cost: 1.00, img: carrot},
+  { name: 'broccoli', cost: 1.80, img: broccoli},
+  { name: 'spinach', cost: 4.10, img: spinach},
+  { name: 'lemon', cost: 0.80, img: lemon},
+  { name: 'mango', cost: 2.00, img: mango},
+  { name: 'avocado', cost: 0.25, img: avocado},
+]
+
+export { inventory }
