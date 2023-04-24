6974ad80680cc28077e38f6415ca2a3aa2f96a47
Mike Melchior <mike.melchior@outlook.com>
2023-04-24 09:42:15 -0400

    add homepage component

---
+++ src/Components/Homepage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Homepage() {
+  return (
+    <div className="homepage">
+      <h2><i>Bringing high quality fresh local produce to your doorstep!</i></h2>
+    </div>
+
+  );
+}
+
+export default Homepage;
