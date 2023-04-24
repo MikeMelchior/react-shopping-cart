6bb0ff0aa80bb782a6d9359ef7527e30292a83c2
Mike Melchior <mike.melchior@outlook.com>
2023-04-24 09:41:11 -0400

    change app component to be used for routing and handle state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
-import React from "react";
+import React, { useState, useEffect  } from "react";
+import { Routes, Route } from "react-router-dom";
+import Cart from "./Components/Cart";
+import Contact from "./Components/Contact";
+import Shop from "./Components/Shop";
+import Homepage from "./Components/Homepage";
 import Nav from "./Components/Nav";
+import ItemCard from './Components/ItemCard'
+import { inventory } from './Inventory/data'
 
-function App() {
+const App = () => {
+
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    let quantityAdded = inventory.map(item => {
+      return {...item, quantity: 0}
+    })
+    setProducts(quantityAdded)
+  }, [])
+
   return (
-
-    <div className="homepage">
+    <>
       <Nav />
-      <h2><i>Bringing high quality fresh local produce to your doorstep!</i></h2>
-    </div>
-  );
+      <div className="container">
+        <Routes>
+          <Route index element={<Homepage />} />
+          <Route path='/shop' >
+            <Route index element={<Shop products={products} />} />
+            <Route path=':id' element={<ItemCard />} />
+          </Route>
+          <Route path="/cart" element={<Cart/>}/>
+          <Route path="/contact" element={<Contact/>} />
+        </Routes>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
