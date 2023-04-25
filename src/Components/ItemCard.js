0896ff91eb9eded3a19d25b6979c552000030914
Mike Melchior <mike.melchior@outlook.com>
2023-04-24 23:42:41 -0400

    start creating itemcard component

--- src/Components/ItemCard.js
+++ src/Components/ItemCard.js
@@ -1,13 +1,45 @@
-import React, { useEffect, useState } from 'react'
+import React, { useRef, useState } from "react"
 
-function ItemCard( { item, setShowCard } ) {
+function ItemCard( { product, cartItems, setCartItems } ) {
+  const [cost, setCost] = useState(product.cost)
+  const [item, setItem] = useState({...product})
 
-  
+  const inputRef = useRef()
+
+  const addToCart = () => {
+    setCartItems(() => {
+      return [...cartItems, item]
+    })
+  }
 
   return (
-    <div className='item-card'>
-      <p onClick={() => setShowCard(false)}> x </p>
-      <img src={item.img} alt="" />
+    <div className="item-card-container">
+      <div className="item-card">
+        <div className="left">
+          <img id="product" src={product.img} alt={product.name} />
+        </div>
+        <div className="right">
+          <h1>{product.name}</h1>
+          <p>CAD ${cost}</p>
+          <div>
+            <button>-</button>
+            <input 
+              ref={inputRef} 
+              onChange={() => {
+                setCost(inputRef.current.value * product.cost)
+                setItem((prev) => {
+                  return {...prev, quantity: inputRef.current.value}
+                })
+              }}
+              type="number" 
+              max={100} 
+              min={0}
+              defaultValue={product.quantity}/>
+            <button>+</button>
+          </div>
+          <button onClick={addToCart}>ADD TO CART</button>
+        </div>
+      </div>
     </div>
   )
 }
